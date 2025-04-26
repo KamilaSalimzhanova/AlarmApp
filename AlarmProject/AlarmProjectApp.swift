39fc9f767c9c1211b0d3c86f25b855b5299e7182
kamila <ksalimzh@gmail.com>
2025-04-26 16:31:56 +0500

    README file resolved issue, localization to 5 languages, and finanlly refactoring folders for better experience

---
+++ AlarmProject/AlarmProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmProjectApp.swift
+//  AlarmProject
+//
+//  Created by kamila on 26.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct AlarmProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
